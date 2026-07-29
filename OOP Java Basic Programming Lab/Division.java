7fd80d719cca23a3c42fc7ed164f27178d86084e
Abidul Islam <abidulislam2828@gmail.com>
2026-07-29 18:29:26 +0600

    Add files via upload

---
+++ OOP Java Basic Programming Lab/Division.java
@@ -0,0 +1,9 @@
+public class Division {
+    public static void main(String[] args) {
+        double num1 = 50;
+        double num2 = 3;
+        double division = num1 / num2;
+
+        System.out.println(division);
+    }
+}
